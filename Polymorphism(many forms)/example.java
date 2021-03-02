c643b47d8c75126444ae68211bc4ca51aa9d02d9
Prineth Fernando <prinethfernandox@gmail.com>
2021-03-02 11:03:53 +0530

    Create example.java

---
+++ Polymorphism(many forms)/example.java
@@ -0,0 +1,18 @@
+class Hello(){
+    public static void main(String []args){
+        System.out.println(Mlp,mult(2,5));
+        System.out.println(Mlp,mult(2.6,4.5));
+    }
+    
+}
+
+class Mlp{
+    static int mult(int a,int b){
+        return a*b;
+    }
+
+
+    static double mult(double a, double b){
+        return a*b;
+    }
+}
